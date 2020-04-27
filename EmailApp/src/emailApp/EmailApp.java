d97be1a9852c154e557b7969a493e3e7b9d17978
Parham <parhampourjam@cmail.carleton.ca>
2020-04-26 22:05:07 -0400

    added project

---
+++ EmailApp/src/emailApp/EmailApp.java
@@ -0,0 +1,12 @@
+package emailApp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Email e1 = new Email("Ichigo", "Kurosaki");
+		
+		e1.showInfo();
+	}
+
+}
