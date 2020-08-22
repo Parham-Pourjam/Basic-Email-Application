7537652e0e16f92161b83888090e48df8c4b25b1
ParhamP <parhampourjam@cmail.carleton.ca>
2020-08-21 23:05:24 -0400

    Update EmailApp.java

--- EmailApp/src/emailApp/EmailApp.java
+++ EmailApp/src/emailApp/EmailApp.java
@@ -3,7 +3,6 @@ package emailApp;
 public class EmailApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Email e1 = new Email("Ichigo", "Kurosaki");
 		
 		e1.showInfo();
